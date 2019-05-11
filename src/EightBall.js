0d62d2a7e01dcd2ed3337dff2e1319aba0475f08
Darius Seals <tristarmediagroupinc@gmail.com>
2019-05-11 09:11:59 -0400

    adjusted size of the ball

--- src/EightBall.js
+++ src/EightBall.js
@@ -3,83 +3,80 @@ import './EightBall.css'
 
 const inputStyle = {
   width: 250,
-
   margin:'2rem',
   borderRadius: 3
-
 }
 
 class EightBall extends Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        userInput: '',
-        randomIndex: ''
-      }
-      this.ask = this.ask.bind(this);
-      this.handleChange = this.handleChange.bind(this)
+  constructor(props){
+    super(props);
+    this.state = {
+      userInput: '',
+      randomIndex: ''
     }
-    ask(){
-      if (this.state.userInput){
-        this.setState({
-          randomIndex: Math.floor(Math.random() * 20),
-          userInput: ''
-        });
-      }
-    }
-    handleChange(event){
+    this.ask = this.ask.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+  }
+  ask(){
+    if (this.state.userInput){
       this.setState({
-        userInput: event.target.value
+        randomIndex: Math.floor(Math.random() * 20),
+        userInput: ''
       });
     }
-    render(){
-      const possibleAnswers = [
-      '"It is certain"',
-      '"It is decidedly so"',
-      '"Without a doubt"',
-      '"Yes, definitely"',
-      '"You may rely on it"',
-      '"As I see it, yes"',
-      '"Outlook good"',
-      '"Yes"',
-      '"Signs point to yes"',
-      '"Reply hazy try again"',
-      '"Ask again later"',
-      '"Better not tell you now"',
-      '"Cannot predict now"',
-      '"Concentrate and ask again"',
-      '"Don\'t count on it"',
-      '"My reply is no"',
-      '"My sources say no"',
-      '"Most likely"',
-      '"Outlook not so good"',
-      '"Very doubtful"'
-    ];
-      const answer = possibleAnswers[this.state.randomIndex];
-      return (
-        <div>
-            <div className="EightBall-outter">
-              <div className="EightBall-inner">
-                <h1 id="eight">8</h1>
-                <h3>Answer:</h3>
-
-                <p>{answer}</p>
-              </div>
-            </div><br/>
-            <h2>Ask any question...</h2>
-            <input
-              type="text"
-              value={this.state.userInput}
-              onChange={this.handleChange}
-              style={inputStyle} />
-
-              <br />
-              <button className="btn" onClick={this.ask}>
-                Press for Answer
-              </button><br />
-        </div>
-      );
-    }
+  }
+  handleChange(event){
+    this.setState({
+      userInput: event.target.value
+    });
+  }
+  render(){
+    const possibleAnswers = [
+    '"It is certain"',
+    '"It is decidedly so"',
+    '"Without a doubt"',
+    '"Yes, definitely"',
+    '"You may rely on it"',
+    '"As I see it, yes"',
+    '"Outlook good"',
+    '"Yes"',
+    '"Signs point to yes"',
+    '"Reply hazy try again"',
+    '"Ask again later"',
+    '"Better not tell you now"',
+    '"Cannot predict now"',
+    '"Concentrate and ask again"',
+    '"Don\'t count on it"',
+    '"My reply is no"',
+    '"My sources say no"',
+    '"Most likely"',
+    '"Outlook not so good"',
+    '"Very doubtful"'
+  ];
+    const answer = possibleAnswers[this.state.randomIndex];
+    return (
+      <div>
+        <div className="EightBall-outter">
+          <div className="EightBall-inner">
+            <h1 id="eight">8</h1>
+            <h3>Answer:</h3>
+            <p>{answer}</p>
+          </div>
+        </div><br/>
+        <h2>Ask any question...</h2>
+        <input
+          type="text"
+          value={this.state.userInput}
+          onChange={this.handleChange}
+          style={inputStyle}
+        />
+        <br />
+        <button className="btn" onClick={this.ask}>
+          Press for Answer
+        </button><br />
+      </div>
+    );
+  }
 }
 
 export default EightBall;
